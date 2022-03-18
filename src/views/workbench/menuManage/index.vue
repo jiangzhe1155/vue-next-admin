8afb83e6d29f5eb71114a08001fb7e8692f4eab5
jiangzhe <1024741733@qq.com>
2022-03-18 23:56:43 +0800

    fix

---
+++ src/views/workbench/menuManage/index.vue
@@ -0,0 +1,257 @@
+<script setup lang="ts">
+// 获取菜单列表
+import {computed, getCurrentInstance, onMounted, reactive, watch, watchEffect} from "vue";
+import {deleteSystem, getSystemMenu, listSystem, MenuDTO, MenuType, SystemDTO, SystemMenuDTO} from "/@/api/userCenter";
+import {useRequest} from "vue-request";
+import AddMenu from '/@/views/workbench/menuManage/component/addMenu.vue';
+import EditMenu from '/@/views/workbench/menuManage/component/editMenu.vue';
+import {ref} from "_vue@3.2.31@vue";
+import {Session} from "/@/utils/storage";
+import {initBackEndControlRoutes} from "/@/router/backEnd";
+import Sortable from 'sortablejs'
+
+const {proxy} = getCurrentInstance() as any;
+
+const state = reactive({
+  systemList: [] as SystemDTO[],
+  currentSelectSystemId: undefined as any,
+  currentName: '' as string,
+  menuTableData: [] as MenuDTO[],
+  size: "default",
+  menuArrayData: [] as MenuDTO[]
+})
+
+const addMenuRef = ref();
+const editMenuRef = ref();
+
+const {loading: listSystemLoading} = useRequest(listSystem, {
+  onSuccess: (data) => {
+    state.systemList = data.data;
+    state.currentSelectSystemId = state.systemList[0].id;
+  }
+});
+
+const {loading: deleteSystemLoading, run: deleteSystemRun} = useRequest(deleteSystem, {
+  manual: true,
+  onSuccess: (data) => {
+    refreshTableData();
+  }
+});
+
+const treeToTile = (treeData: any) => {
+  let arr = []
+  const expanded = data => {
+    if (data && data.length > 0) {
+      data.filter(d => d).forEach(e => {
+        arr.push(e)
+        expanded(e.children || [])
+      })
+    }
+  }
+  expanded(treeData)
+  return arr;
+}
+
+
+const {
+  loading: getSystemMenuLoading,
+  run: getSystemMenuRun
+} = useRequest(getSystemMenu, {
+  manual: true, onSuccess: (data) => {
+    let systemMenu = data.data as SystemMenuDTO
+    state.menuTableData = systemMenu.menus;
+    state.menuArrayData = treeToTile(systemMenu.menus);
+  }
+});
+
+const onOpenAddMenu = () => {
+  addMenuRef.value.openDialog();
+}
+
+const onOpenAddSubMenu = (row: any) => {
+  addMenuRef.value.state.ruleForm.pid = row.id;
+  addMenuRef.value.state.isParentMenuSelectDisable = true;
+  addMenuRef.value.openDialog();
+}
+
+
+const onOpenEditMenu = (row) => {
+  let ruleForm = editMenuRef.value.state.ruleForm;
+  ruleForm.id = row.id;
+  ruleForm.pid = row.pid;
+  ruleForm.name = row.name;
+  ruleForm.type = row.type;
+  ruleForm.icon = row.icon;
+  ruleForm.code = row.code;
+  ruleForm.component = row.component;
+
+  // 是否禁用选项
+  editMenuRef.value.state.isMenuTypeDisable = row.children && row.children.length > 0;
+  editMenuRef.value.openDialog(row);
+}
+
+
+const onTableRowDel = (row: { id: any; }) => {
+  deleteSystemRun({id: row.id});
+}
+
+const getMenuData = computed(() => {
+  return dfsFilter(state.menuTableData);
+})
+
+function dfsFilter(menuTableData: MenuDTO[]) {
+  if (!menuTableData) {
+    return [];
+  }
+
+  return menuTableData.map(menu => {
+    let res: MenuDTO = Object.assign({}, menu);
+    if (res.children) {
+      res.children = dfsFilter(menu.children);
+    }
+    return res;
+  }).filter(a => {
+    if (!state.currentName) {
+      return true;
+    }
+    return (a.children && a.children.length > 0 || (a.name.indexOf(state.currentName) != -1));
+  })
+}
+
+const refreshTableData = () => {
+  getSystemMenuRun({systemId: state.currentSelectSystemId}).then(() => {
+    // 如果
+    if (Session.get('systemId') === state.currentSelectSystemId) {
+      //初始化路由
+      initBackEndControlRoutes();
+      //初始化菜單
+      proxy.mittBus.emit('getBreadcrumbIndexSetFilterRoutes');
+    }
+  })
+}
+
+watch(() => state.currentSelectSystemId, (systemId, oldSystemId) => {
+  // 系统id发生改变，重新获取菜单
+  getSystemMenuRun({systemId: systemId}).catch(() => {
+    state.currentSelectSystemId = oldSystemId;
+  });
+})
+
+
+// onMounted(() => {
+//   const tbody = document.querySelector('.el-table__body-wrapper tbody')
+//   Sortable.create(tbody, {
+//     // ghostClass: 'sortable-ghost',
+//
+//     onMove: function ({dragged, related}) {
+//       console.log(dragged, related);
+//       const oldRow = state.menuArrayData[dragged.rowIndex]
+//       const newRow = state.menuArrayData[related.rowIndex]
+//       if (oldRow.level !== newRow.level || oldRow.pid !== newRow.pid) {
+//         return false
+//       }
+//     },
+//     onEnd({newIndex, oldIndex}) {
+//       console.log(newIndex, oldIndex)
+//     }
+//   })
+// })
+
+
+</script>
+<template>
+  <div class="system-menu-container">
+    <el-card shadow="hover">
+      <div class="system-menu-search mb15">
+        <el-select :size="state.size" style="max-width: 180px" class="ml10" v-model="state.currentSelectSystemId">
+          <el-option
+              v-for="item in state.systemList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+          />
+        </el-select>
+        <el-input :size="state.size" v-model="state.currentName" placeholder="请输入菜单名称" style="max-width: 180px"
+                  class="ml10"></el-input>
+        <!--        <el-button :size="state.size" type="primary" class="ml10" @click="onSearch()">-->
+        <!--          <el-icon>-->
+        <!--            <elementSearch/>-->
+        <!--          </el-icon>-->
+        <!--          查询-->
+        <!--        </el-button>-->
+        <el-button :size="state.size" type="success" class="ml10" @click="onOpenAddMenu">
+          <el-icon>
+            <elementFolderAdd/>
+          </el-icon>
+          新增菜单
+        </el-button>
+      </div>
+      <el-table v-loading="getSystemMenuLoading" :data="getMenuData" style="width: 100%"
+                row-key="id"
+                :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
+        <el-table-column label="菜单名称" show-overflow-tooltip>
+          <template #default="scope">
+            <SvgIcon :name="scope.row.icon"/>
+            <span class="ml10">{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="path" label="路径" show-overflow-tooltip></el-table-column>
+        <el-table-column label="组件路径" show-overflow-tooltip>
+          <template #default="scope">
+            <span>{{ scope.row.component }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="类型" show-overflow-tooltip width="100">
+          <template #default="scope">
+            <el-tag type="success" v-if="scope.row.type ==='menu'"> {{ scope.row.typeName }}</el-tag>
+            <el-tag v-if="scope.row.type ==='menuGroup'">{{ scope.row.typeName }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="创建时间" show-overflow-tooltip>
+          <template #default="scope">
+            <span>{{ scope.row.createTime }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" show-overflow-tooltip>
+          <template #default="scope">
+            <el-button v-if="scope.row.type === MenuType.menuGroup" size="small" type="text"
+                       @click="onOpenAddSubMenu(scope.row)">新增子菜单
+            </el-button>
+            <el-button size="small" type="text" @click="onOpenEditMenu(scope.row)">修改</el-button>
+            <el-popconfirm title="确认删除?" @confirm="onTableRowDel(scope.row)">
+              <template #reference>
+                <el-button size="small" type="text">删除
+                </el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+    <AddMenu ref="addMenuRef" v-model:menus="state.menuTableData" v-model:systemId="state.currentSelectSystemId"
+             @refreshTableData="refreshTableData"/>
+    <EditMenu ref="editMenuRef" v-model:menus="state.menuTableData" @refreshTableData="refreshTableData"/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "menuManage"
+}
+</script>
+
+<style>
+.demo-tabs > .el-tabs__content {
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+
+.demo-tabs > .el-tabs__header {
+  border-right-width: 1px;
+}
+
+.el-tabs--left .el-tabs__content {
+  height: 100%;
+}
+</style>
